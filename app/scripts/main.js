4cf833068040db37995e2f99145b8cb3623b3e53
Matt Daniel Brown <developer@mattdanielbrown.com>
2020-04-02 14:53:16 -0500

    Lots of changes. Worked on Docs too

---
+++ app/scripts/main.js
@@ -0,0 +1,4 @@
+const gitHubLinkElement = document.querySelector('.github-link');
+
+gitHubLinkElement.setAttribute('href', 'https://github.com/mattdanielbrown/scss-button-mixins');
+
--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const path = require('path');
-
-module.exports = {
-	includePaths: [ path.join(__dirname, 'stylesheets') ]
-};
